46c4db2c86daa90e1ffb50d2bd291d56db6c5db6
gvestmann <gvestmann@icloud.com>
2021-01-28 14:58:45 +0100

    Some more stuff

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request, redirect
+from data import Data
 
 
 app = Flask(__name__)
 
+Data = Data()
 
 # route to mainpage
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# @app.route("/dogs")
+# def articles():
+#     return render_template("dogs.html", dogs=dogs)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,24 @@
+def Data():
+    [
+        {
+            'id': 1,
+            'name': 'Dog 1',
+            'body': 'Lorem ipsum',
+            'author': 'Guðmundur',
+            'create_data': '28-01-2021'
+        },
+        {
+            'id': 2,
+            'name': 'Dog 2',
+            'body': 'Lorem ipsum',
+            'author': 'Guðmundur',
+            'create_data': '28-01-2021'
+        },
+        {
+            'id': 2,
+            'name': 'Dog 2',
+            'body': 'Lorem ipsum',
+            'author': 'Guðmundur',
+            'create_data': '28-01-2021'
+        }
+    ]
